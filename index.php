038047bdb2057c4ba3ce67a0aa5e92a5744f370f
uilight <juankmussyto@gmail.com>
2015-02-08 00:33:11 -0600

    [start-foundation1] - Add basic HTML - Add Foundation files

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]>
+<html lang="en" class="no-js ie6">
+<![endif]-->
+
+<!--[if IE 7 ]>
+<html lang="en" class="no-js ie7">
+<![endif]-->
+
+<!--[if IE 8 ]>
+<html lang="en" class="no-js ie8">
+<![endif]-->
+
+<!--[if IE 9 ]>
+<html lang="en" class="no-js ie9">
+<![endif]-->
+
+<!--[if (gt IE 9)|!(IE)]>
+<!--> <html lang="en" class="no-js">
+<!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">    
+    <meta name="format-detection" content="telephone=no">
+    <meta name="x-rim-auto-match" content="none">
 	<title></title>
 	<link rel="stylesheet" href="">
+    <link rel="stylesheet" href="foundation/css/foundation.css" />
+    <script src="foundation/js/vendor/modernizr.js"></script>
+	<!--[if lt IE 9]>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript" language="javascript"></script>
+	<![endif]-->
 </head>
 <body>
-	
+
+    <script src="foundation/js/vendor/jquery.js"></script>
+    <script src="foundation/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
 </body>
 </html>
